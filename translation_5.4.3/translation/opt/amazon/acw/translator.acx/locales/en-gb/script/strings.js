d3d28d4c69583dfe03fa87075740051f06f2155c
danan72 <danan72@wp.pl>
2014-03-08 23:59:09 +0100

    translation 5.4.3

---
+++ translation_5.4.3/translation/opt/amazon/acw/translator.acx/locales/en-gb/script/strings.js
@@ -0,0 +1,41 @@
+About Translation	O tłumaczeniu
+Aeroplane Mode must be turned off to use this feature.	Aby skorzystać z tej funkcji trzeba wyłączyć tryb samolotowy.  
+An error occurred. Please try again.	Wystąpił błąd. Spróbuj ponownie później.
+Back to Translation	Wróć do tłumaczenia
+Chinese Simplified	Chiński prosty
+Chinese Traditional	Chiński tradycyjny
+Czech	Czeski
+Danish	Duński
+Detecting...	Wykrywanie...
+Dutch	Holenderski
+English	Angielski
+Finnish	Fiński
+French	Francuzki
+From:	Z:
+German	Niemiecki
+Greek	Grecki
+Hebrew	Hebrajski
+Hindi	Hinduski
+Italian	Włoski
+Japanese	Japoński
+Korean	Koreański
+Latin	Łaciński
+No network detected. Please check your connection and try again.	Nie znaleziono żadnej sieci. Sprawdź swoje połączenie i spróbuj ponownie.
+No text was selected for translation.	Nie został wybrany tekst do przetłumaczenia.
+Norwegian	Norweski
+Polish	Polski
+Portuguese	Portugalski
+Russian	Rosyjski
+Slovak	Słowacki
+Spanish	Hiszpański
+To:	Na:
+Translating your selection...	Tłumaczę wybrany tekst...
+Translation	Tłumaczenie
+Turkish	Turecki
+Turn Off Aeroplane Mode	Wyłącz tryb samolotowy
+Turn On Wireless	Włącz łączność bezprzewodową
+Ukrainian	Ukraiński
+Unable to contact the server. Please try again later.	Nie można połączyć się z serwerem. Spróbuj ponownie później.
+Unknown	Nieznany
+Waiting for connection...	Czekam na połączenie
+Wireless must be turned on to use this feature.	Aby użyć tej funkcji, musi być włączona łączność bezprzewodowa.
---
+++ translation_5.4.3/translation/opt/var/local/mesquite/shared/locales/en-GB/js/strings.js
@@ -0,0 +1,16 @@
+Cancel	Anuluj
+Close	Zamknij
+Connection Required	Wymagane jest połączenie
+Edit	Edytuj
+Lock Rotation	Blokada rotacji
+Menu	Menu
+OK	OK
+Register	Rejestruj
+Remove	Usuń
+Shop in Kindle Store	Zakupy w Kindle Store
+This action requires wireless to be turned on.	Ta czynność wymaga włączenia łączności bezprzewodowej.
+Turn Off Wireless	Wyłącz połączenia bezprzewodowe
+Turn On Wireless	Włącz połączenia bezprzewodowe
+Unable to Connect	Nie można się połączyć
+Unlock Rotation	Odblokowanie rotacji
+Your Kindle is currently unable to connect.<br><br>Please try again later.	Twój Kindle nie może się w tej chwili połączyć.<br><br>Spróbuj ponownie później.
