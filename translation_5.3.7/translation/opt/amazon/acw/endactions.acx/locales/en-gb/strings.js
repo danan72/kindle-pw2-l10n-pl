fa54692acf32ca00f3392e5a745d5f8e2d29022e
danan72 <danan72@wp.pl>
2014-04-08 16:24:39 +0200

    revoke incorrect changes

---
+++ translation_5.3.7/translation/opt/amazon/acw/endactions.acx/locales/en-gb/strings.js
@@ -0,0 +1,17 @@
+Before you go...	Zanim pójdziesz
+Buy Now	Kup teraz
+Close this window to return to the book	Zamknij okno i wróć do książki
+Customers who bought this book also bought	Klienci, którzy kupili tą książkę, kupili również
+More books by %{authors}	Więcej książek autorstwa %{authors}
+Rate this book	Oceń tą książkę
+See in Store	Zobacz w sklepie
+Share	Udostępnij
+Share you've finished	Pochwal się, że przeczytałeś
+Your rating	Twoja ocena
+finished	przeczytana
+finished %{title}	książka %{title} została przeczytana
+finished %{title} and gave it %{num} stars	książka %{title} została przeczytana, przyznana liczba gwiazdek: %{num}
+finished %{title} and gave it 1 star	książka %{title} została przeczytana, przyznana liczba gwiazdek: 1
+finished %{title} by %{authors}	książka %{title} autorstwa %{authors} została przeczytana
+finished %{title} by %{authors} and gave it %{num} stars	książka %{title} autorstwa %{authors} została przeczytana, przyznana liczba gwiazdek: %{num}
+finished %{title} by %{authors} and gave it 1 star	książka %{title} autorstwa %{authors} została przeczytana, przyznana liczba gwiazdek: 1
