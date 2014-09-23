d1ca92bc78caed692d7640e0af9e56bdf5f12516
danan72 <danan72@wp.pl>
2014-09-23 10:14:10 +0200

    Update password_dialog_strings.js

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/password_dialog_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/password_dialog_strings.js
@@ -4,7 +4,7 @@ If you have forgotten your passcode, please contact Kindle Customer Service at w
 Incorrect Passcode	Niepoprawny kod dostępu
 OK	OK
 Passcode Required	Wymagany jest kod dostępu
-Passcode hint: 	Wskazówka do kodu dostępu:
+Passcode hint: 	Wskazówka dla kodu dostępu:
 The passcode you entered is incorrect. Please try again.	Wprowadzony kod dostępu jest nieprawidłowy. Spróbuj jeszcze raz.
 This device has been disabled remotely.	To urządzenie zostało zdalnie wyłączone.
 Your Kindle will reset to factory defaults after 2 more failed attempts.	Po kolejnych dwóch nieudanych próbach zostaną przywrócone ustawienia fabryczne.
